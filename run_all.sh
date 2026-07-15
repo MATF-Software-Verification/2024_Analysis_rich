e3497adb28c92a8b812e39a20405699a257b06d1
jovan <jovanran1997@gmail.com>
2026-07-15 19:38:26 +0200

    add run_all.sh

---
+++ run_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+echo "========================================="
+echo "Running all analysis"
+echo "========================================="
+echo ""
+./pytest/run_pytest.sh
+echo ""
+./coverage/run_coverage.sh
+echo ""
+./pylint/run_pylint.sh
+echo ""
+./mypy/run_mypy.sh
+echo ""
+./radon/run_radon.sh
+echo ""
+./vulture/run_vulture.sh
+echo ""
+./interrogate/run_interrogate.sh
+echo ""
+echo "========================================="
+echo "Analysis completed!"
+echo "========================================="
